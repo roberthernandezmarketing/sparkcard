c5a1ad8e90fac7f1351844335c518f1361ac6061
Rob Data <roberthernandezmarketing@gmail.com>
2025-06-15 22:44:06 -0400

    Login/Authorization

--- backend/src/crud/user_crud.py
+++ backend/src/crud/user_crud.py
@@ -4,19 +4,37 @@
 # Implements CRUD operations for the USer model. Handles direct interaction with 
 # the Users table in the database.
 #
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 from backend.src.models.user_model import User
+from backend.src.schemas.user_schema import UserCreate
 from backend.src.utils.security import get_password_hash
+import uuid
 
-async def create_user(db: AsyncSession, username: str, email: str, password: str):
-    hashed_password = get_password_hash(password)
-    new_user = User(username=username, email=email, hashed_password=hashed_password)
+
+async def get_user_by_username(db: AsyncSession, username: str):
+    result = await db.execute(select(User).where(User.user_name == username))
+    return result.scalars().first()
+
+
+async def get_user_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(User).where(User.user_email == email))
+    return result.scalars().first()
+
+
+async def create_user(db: AsyncSession, user_data: UserCreate) -> User:
+    hashed_password = get_password_hash(user_data.password)
+    new_user = User(
+        user_name=user_data.username,
+        user_email=user_data.email,
+        user_password_hash=hashed_password,
+    )
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
     return new_user
 
-async def get_user_by_username(db: AsyncSession, username: str):
-    result = await db.execute(select(User).where(User.username == username))
+
+async def get_user_by_id(db: AsyncSession, user_id: uuid.UUID):
+    result = await db.execute(select(User).where(User.user_id == user_id))
     return result.scalars().first()
